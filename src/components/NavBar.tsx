0f2568530eaf901e4792409a29dc14deb4573e7a
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2024-05-19 14:36:27 -0300

    created lnks

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,41 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
+interface Link {
+  title: string;
+  src: string;
+}
+
+const links: Link[] = [
+  {
+    title: "Home",
+    src: "/",
+  },
+  {
+    title: "Properties",
+    src: "/properties",
+  },
+  {
+    title: "Map",
+    src: "/map",
+  },
+];
+
 const NavBar = (props: Props) => {
-  return <div>NavBar</div>;
+  return (
+    <div className="h-16 w-full flex flex-column sm:flex-row ">
+      <Link href={"/"}>
+        <h1>HavenHill Homes</h1>
+      </Link>
+      {links.map((l) => (
+        <Link key={l.src} href={l.src}>
+          {l.title}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default NavBar;
