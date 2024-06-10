de2af373a68c38b1c445f2333ba9e23a00d53b7d
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2024-06-10 18:58:42 -0300

    search bar UI

--- src/app/(user)/(routes)/properties/page.tsx
+++ src/app/(user)/(routes)/properties/page.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { homes } from "../../../../../home";
 import Image from "next/image";
+import { Search } from "lucide-react";
 
 type Props = {};
 
 const Properties = (props: Props) => {
   return (
-    <div className="flex h-full  sm:flex-row flex-col w-full items-center justify-around   gap-8  flex-wrap">
-      {homes.map((home) => (
-        <div className="flex p-4 h-[200px] w-[200px]" key={home.id}>
-          <Image
-            alt="home"
-            src={home.thumbnail}
-            className="flex object-cover"
-          />
-        </div>
-      ))}
-    </div>
+    <section className="flex flex-col items-center">
+      <div className="flex  items-center h-8 pb-20">
+        <input type="text" className="flex text-black  mr-2" />
+        <Search size={20} color="black" cursor={"pointer"} />
+      </div>
+      <div className="flex h-full  sm:flex-row flex-col w-full items-center justify-around px-[20%]  gap-8  flex-wrap">
+        {homes.map((home) => (
+          <div className="flex p-4 h-[200px] w-[200px]" key={home.id}>
+            <Image
+              alt="home"
+              src={home.thumbnail}
+              className="flex object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
