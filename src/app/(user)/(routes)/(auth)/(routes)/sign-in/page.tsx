fe1920abc4859ba546ae285aad409d95ff44dcad
JustoMateos21 <98715129+JustoMateos21@users.noreply.github.com>
2024-07-04 18:28:45 -0300

    auth system

---
+++ src/app/(user)/(routes)/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+type Props = {};
+
+const SigninPage = (props: Props) => {
+  return <div></div>;
+};
+
+export default SigninPage;
